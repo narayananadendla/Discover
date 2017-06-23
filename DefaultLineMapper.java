6dd436986a49f8cafcba8253101a2e41e05b1b76
narayananadendla <narayana4nadendla@gmail.com>
2017-06-23 12:17:15 +0530

    Add files via upload

---
+++ DefaultLineMapper.java
@@ -0,0 +1,10 @@
+package com.cts.sample;
+
+public class DefaultLineMapper {
+	
+	public void setLineTokenizer(){
+		System.out.println("defaultLineMapper");
+		
+	}
+
+}
---
+++ FlatFileItemReader.java
@@ -0,0 +1,13 @@
+package com.cts.sample;
+
+public class FlatFileItemReader {
+	
+	public void setLineMapper(DefaultLineMapper dft) {
+		System.out.println("flatfileItemReader");
+
+	}
+	public void setCount(int i){
+		
+	}
+
+}
